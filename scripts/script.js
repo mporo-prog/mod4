e78515f39ccc6e28f936e7a5d38929855e5bf715
Mporo <polinamporo@gmail.com>
2026-06-21 20:42:40 +0300

    Add all pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    burgerMenu = document.querySelector("[data-test='burgerMenu']");
+    burgerMenu.addEventListener("click", () => {
+        const burgerButton = document.querySelector("[data-test='burgerButton']");
+        const currentUrl = encodeURIComponent(window.location.href);
+        burgerButton.href = `menu.html?return=${currentUrl}`;
+    })
 });
