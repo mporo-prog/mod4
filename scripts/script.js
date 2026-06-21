fabf541dea565c4f4c283f0a03bb3fd257d6ed08
Mporo <polinamporo@gmail.com>
2026-06-22 00:58:56 +0300

    Add addaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    burgerMenu = document.querySelector("[data-test='burgerMenu']");
-    burgerMenu.addEventListener("click", () => {
-        const burgerButton = document.querySelector("[data-test='burgerButton']");
-        const currentUrl = encodeURIComponent(window.location.href);
-        burgerButton.href = `menu.html?return=${currentUrl}`;
-    })
+    
 });
